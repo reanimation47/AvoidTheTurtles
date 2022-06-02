5d0a42acf314a5c7e6f50324330ce48d95505737
DannnyLe <danny.le@playstudios.asia>
2022-06-02 14:11:40 +0700

    added energy bar animation when full and basic highscore display ingame

--- Assets/Scripts/GameUImanager.cs
+++ Assets/Scripts/GameUImanager.cs
@@ -8,18 +8,24 @@ public class GameUImanager : MonoBehaviour
 {
     public static GameUImanager instance {get; private set;}
 
+    //Energy slider
     [SerializeField] private GameObject energySlider;
     [SerializeField] private GameObject energySliderFill;
     Color energySliderFillColor;
-
+    Vector3 energyBarScaler = new Vector3(1,1.5f,1);
     float energySliderValue;
     float energyPoints = 0;
+    public bool energyBarWiggling = false;
+    public bool energyBarWiggleOn = false;
 
     //Score
     [SerializeField] private GameObject ScoreDisplay;
     public TextMeshProUGUI scoreText;
     public bool ScorePop = false;
     Vector3 ScoreScaler = new Vector3(1,1,1);
+
+    //HighScore
+    [SerializeField] private TextMeshProUGUI currentHighScoreText;
 
     private void Awake()
     {
@@ -44,6 +50,9 @@ public class GameUImanager : MonoBehaviour
 
     void Update()
     {
+
+        currentHighScoreText.text = MenuManager.instance.currentHighscore().ToString();
+        //Animating Energy bar
         energySliderFill.GetComponent<Image>().color = energySliderFillColor;
         energyPoints = GameManager.instance.specialPoints;
         energySlider.GetComponent<Slider>().value= Mathf.Lerp(energySlider.GetComponent<Slider>().value, energyPoints, 0.05f);
@@ -51,6 +60,30 @@ public class GameUImanager : MonoBehaviour
         energySliderFillColor.r = Mathf.Lerp(energySliderFillColor.r, energyPoints/5, 0.05f);
 
         scoreText.text = GameManager.instance.points.ToString();
+
+        //Animating Enery bar when energy reaches full
+        energySlider.transform.localScale = energyBarScaler;
+        if(energyPoints == 5 && !energyBarWiggling)
+        {
+            energyBarWiggling = true;
+            StartCoroutine(wiggleEnergyBar(0.2f));
+            
+        }
+        if(energyPoints != 5 && energyBarWiggling)
+        {
+            StopCoroutine(wiggleEnergyBar(0.2f));
+            energyBarWiggleOn = false;
+            energyBarWiggling = false;
+        }
+        if(energyBarWiggleOn)
+        {
+            energyBarScaler.x = Mathf.Lerp(energyBarScaler.x,1.1f,0.05f);
+            energyBarScaler.y = Mathf.Lerp(energyBarScaler.y,1.6f,0.05f);
+        }else
+        {
+            energyBarScaler.x = Mathf.Lerp(energyBarScaler.x,1f,0.05f);
+            energyBarScaler.y = Mathf.Lerp(energyBarScaler.y,1.5f,0.05f);
+        }
 
 
         //Animating current score
@@ -76,5 +109,15 @@ public class GameUImanager : MonoBehaviour
     public void startWigglingScoreDisplay()
     {
         StartCoroutine(wiggleScoreDisplay());
+    }
+
+    IEnumerator wiggleEnergyBar(float timeGap)
+    {
+        while(energyBarWiggling)
+        {
+            energyBarWiggleOn = energyBarWiggleOn ? false : true;
+            yield return new WaitForSeconds(timeGap);
+        }
+        
     }
 }
